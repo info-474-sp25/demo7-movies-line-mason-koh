10612449410a0f637d73a6cba09c3974d7d7e9a6
masonkoh1 <masonkoh@gmail.com>
2025-05-19 16:12:20 -0700

    added bar chart

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const margin = { top: 50, right: 30, bottom: 60, left: 70 },
       height = 400 - margin.top - margin.bottom;
 
 // 1: CREATE SVG CONTAINERS
-// 1: Line Chart Container
+// Line Chart Container
 const svgLine = d3.select("#lineChart")
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Bar Chart Container
+const svgBar = d3.select("#barChart")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
@@ -15,6 +20,8 @@ d3.csv("movies.csv").then(data => {
     data.forEach(d => {
         d.gross = +d.gross;   // Convert score to a number
         d.year = +d.title_year;    // Convert year to a number
+        d.director = d.director_name;
+        d.score = d.imdb_score;
     });
 
     // Check your work
@@ -29,7 +36,7 @@ d3.csv("movies.csv").then(data => {
 	    && d.year >= 2010   // Filter for years >= 2010
     );
 
-    console.log(cleanData);
+    // console.log(cleanData);
     
     // 3.b: Group by and summarize (aggregate gross by year)
     const dataMap = d3.rollup(cleanData,
@@ -37,7 +44,7 @@ d3.csv("movies.csv").then(data => {
         d => d.year
     );
     
-    console.log(dataMap);
+    // console.log(dataMap);
 
     // 3.c: Convert to array and sort by year
     const dataArr = Array.from(dataMap,
@@ -46,7 +53,7 @@ d3.csv("movies.csv").then(data => {
         .sort((a, b) => a.year - b.year)
     ;
 
-    console.log(dataArr);
+    // console.log(dataArr);
 
     // Check your work
     // console.log(lineData);
@@ -121,4 +128,90 @@ d3.csv("movies.csv").then(data => {
         .attr("y", (-margin.left / 2) - 10)
         .attr("x", -height / 2)
         .text("Gross Revenue ($)");
+
+    /* ===================== BAR CHART ===================== */
+    // 3. PREPARE Bar DATA
+    // 3.a Clean data
+    const barCleanData = data.filter(d =>
+        d.director != ''
+        && d.score != null
+    );
+
+    console.log("Clean bar data: ", barCleanData);
+    
+    // 3.b Group by [director] & aggregate [score]
+    const barMap = d3.rollup(barCleanData
+        ,v => d3.mean(v, d => d.score)
+        ,d => d.director
+    );
+    
+    console.log("Bar: ", barMap);
+
+    // 3.c Sort & get top 6
+    const barFinalData = Array.from(barMap, // convert to array
+            ([director, score]) => ({ director, score })
+        )
+        .sort((a, b) => b.score - a.score) // sort by score, desc
+        .slice(0, 6)  
+    ;
+
+    console.log("Final bar data: ", barFinalData);
+
+    // 4: SCALE AXES
+    // 4.a: x-axis (director)
+    let barXScale = d3.scaleBand() // Use instead of scaleLinear() for bar charts
+        .domain(barFinalData.map(d => d.director)) // Extract unique categories for x-axis
+        .range([0, width]) // START low, INCREASE
+        .padding(0.1); // Add space between bars
+
+    // 4.b: y-axis (score)
+    let barYScale = d3.scaleLinear()
+        .domain([0, d3.max(barFinalData, d => d.score)])
+        .range([height,0]); // START high, DECREASE
+
+    // 5: PLOT DATA
+    svgBar.selectAll("rect")
+        .data(barFinalData)
+        .enter()
+        .append("rect")
+        .attr("x", d => barXScale(d.director))
+        .attr("y", d => barYScale(d.score))
+        .attr("width", barXScale.bandwidth())
+        .attr("height", d => height - barYScale(d.score))
+        .attr("fill", "blue")
+        ;
+    
+    // 6: ADD AXES
+    // 6.a: x-axis
+    svgBar.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(barXScale));
+
+    // 6.b: y-axis
+    svgBar.append("g")
+        .call(d3.axisLeft(barYScale));
+
+    // 7: ADD LABELS
+    // 7.a: title
+    svgBar.append("text")
+        .attr("class", "title")
+        .attr("x", width / 2)
+        .attr("y", -margin.top / 2)
+        .text("Top 6 Director's IMDb Scores");
+
+    // 7.b: x-axis
+    svgBar.append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + (margin.bottom / 2) + 10)
+        .text("Directors");
+    
+        // 7.c: y-axis
+    svgBar.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left / 2)
+        .attr("x", -height / 2)
+        .text("Score");
+
 });
